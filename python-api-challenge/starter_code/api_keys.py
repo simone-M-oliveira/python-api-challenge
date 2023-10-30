00f2ddf19f5f63ae5789e4a167edb7f2ae450bab
simone-M-oliveira <118147206+simone-M-oliveira@users.noreply.github.com>
2023-10-29 19:47:22 -0500

    Update api_keys.py

--- python-api-challenge/starter_code/api_keys.py
+++ python-api-challenge/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "0bd32ed9e1bc586d376b4b5093abfca5"
+weather_api_key = "0e7d083a62ee45989cfccd6e82b48abf"
 
 # Geoapify API Key
 geoapify_key = "19b7318a78c8411b82aae39ac2984e9a"
